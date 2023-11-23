ab7c348b29ffe1958ce676c7a93ccfc2602d0dd1
Ronald Coe Jr <rcoe@miosys.org>
2023-11-22 21:46:54 -0500

    Working implementation of multi-sheet excel file import--with sheets that have spaces in their names!

--- app-excel-import.py
+++ app-excel-import.py
@@ -7,8 +7,11 @@ from collections import OrderedDict
 from datetime import datetime
 import xmltodict
 import pandas as pd
+import polars as pl
+import modin.pandas
 import os
 import streamlit as st
+import re
 
 
 
@@ -59,22 +62,29 @@ def write_excel_to_table(input_file_name):
 
 
     # Get the number of dictionary keys that would result in a call to read_excel() with sheet_name=None
-    n_sheets = len(pd.read_excel(input_file_name, sheet_name=None).keys())
-
+    # n_sheets = len(pl.read_excel(input_file_name, sheet_name=None, sheet_id=0).keys())
+    s = pl.read_excel(input_file_name, sheet_id=0)
+    
     # Iterate through the sheets in the workbook, referencing them by index
     # We are using this method because a call to read_excel() with sheet_name=None results
     # in a problematic dictionary if any of the sheets have non-alphanumeric characters
-    for i in range(n_sheets):
-        s = pd.read_excel(input_file_name, sheet_name=i)
-        i_name = "{}".format(i)
-        temp_df = s
+    for i in s:
+        # st.write(s["Sheet1"])
+        # s = pl.read_excel(input_file_name, )
+        i_name = "{}".format(i.title())
+        temp_df = s[i]
         try:       
-            my_df = session.createDataFrame(temp_df)
-            t_index = output_table_name + "_" + i_name
-            t_output_table_name[t_index] = output_table_name + "_" + i_name
+            # if len(s) > 1:
+            #     my_df = session.createDataFrame(pl.DataFrame()._from_dict(temp_df))
+            # else:
+            my_df = session.createDataFrame(temp_df.to_pandas())
+            t_index = output_table_name + "_{}".format(re.sub(' ', '_', i))
+            t_output_table_name[t_index] = t_index
             my_df5 = my_df.write.mode("overwrite").save_as_table(table_name=t_output_table_name[t_index], table_type='transient')
+            output_table_name = os.environ["excel_table_prefix"]
         except ValueError:
             st.write("Skipping the sheet named: '{}'.  Pandas read_excel() dictionary creation problem.".format(i_name))
+            output_table_name = os.environ["excel_table_prefix"]
 
     return t_output_table_name
 
@@ -95,6 +105,7 @@ if ((uploaded_files)):
             for t_table in t_arr:
                 try:
                     t_df = m_session1.table(t_table)
+                    st.write(t_table)
                     st.dataframe(t_df.limit(25).toPandas())
                 except:
                     st.write("The table you tried to open doesn't exist.")
